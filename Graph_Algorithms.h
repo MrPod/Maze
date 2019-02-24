7e4a4cb9ef5f2245e817956174e2c7d7a0a29374
Sasha <pod.haulm@gmail.com>
2019-02-25 01:59:52 +0300

    Update Graph_Algorithms.h

--- Graph_Algorithms.h
+++ Graph_Algorithms.h
@@ -11,12 +11,7 @@ struct Edge
   int start, end, weight;
 };
 
-
-const int N = 4, M = 4;
 const double AE = 0.08;
 
-
-bool Dijkstra( int begin, int end, int arr[N * M][N * M] );
-int Li( int begin, int end, int arr[N * M][N * M] );
 Cell** Maze_Builder( int A, int B );
 void Game(Cell** paths, int A, int B);
